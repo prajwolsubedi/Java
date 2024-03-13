72d4448bca149a1fde7c51a81e543243554b916a
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-03-13 09:52:51 +0545

    wrapper class

---
+++ First/src/ExampleWrapperClass.java
@@ -0,0 +1,12 @@
+public class ExampleWrapperClass {
+
+    public static void main(String args[]) {
+        int rollNo = 1;
+        Integer rollNo1 = 12;
+//        System.out.print(rollNo1.equals(12));
+
+        int result = rollNo1.intValue();
+        System.out.println(result);
+
+    }
+}
