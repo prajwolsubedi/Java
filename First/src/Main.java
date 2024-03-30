a46f7c7c64b6ba3fbe635f249ad4b7151db69717
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-03-30 21:56:20 +0545

    update

--- First/src/Main.java
+++ First/src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        /***Polymorphism
+         * Car car = new Car();
+         *         Bicycle bicycle = new Bicycle();
+         *         Boat boat = new Boat();
+         *         Vehicle[] transport = {car, bicycle,boat};
+         *
+         *         for(Vehicle x : transport){
+         *             x.go();
+         *         }
+         *
+         *
+         *
+         */
+
+
+        /*
+        //Audio
+                Scanner scanner = new Scanner(System.in);
+
+        File file = new File("example.wav");
+        AudioInputStream audioInput = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInput);
+        String response = "";
+
+        while (!response.equals("Q")){
+            System.out.println("P = Play, Q = Quit, S = Stop, R = Reset");
+            System.out.println("Enter your choice: ");
+            response = scanner.next();
+            response = response.toUpperCase();
+            switch (response){
+                case "P": clip.start();
+                    break;
+                case ("Q"): clip.stop();
+                    break;
+                case ("S"): clip.close();
+                    break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                    break;
+                default: System.out.println("Not a valid response");
+            }
         }
+        System.out.println("Out of the loop Byeee!!");
+
+         */
+
+
+
+
+
     }
 }
